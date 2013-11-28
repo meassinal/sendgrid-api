05ec4f2b54483f5ea37449f3b662140d43719d46
sinal <meassinal@gmail.com>
2013-11-28 09:53:07 +0700

    add various fields to email

--- lib/sendgrid/api/entities/email.rb
+++ lib/sendgrid/api/entities/email.rb
@@ -7,7 +7,9 @@ module Sendgrid
 
         attribute :email, :name, :listing_name, :listing_logo_url, 
                   :listing_communications, :listing_extra_lines,
-                  :listing_info
+                  :listing_info, :listing_categories, :listing_descriptions,
+                  :listing_keywords, :listing_relations, :listing_qrcode,
+                  :listing_video
 
       end
     end
