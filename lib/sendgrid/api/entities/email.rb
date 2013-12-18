074bf412f3aa05f548f272181cb575750012f03a
sinal <meassinal@gmail.com>
2013-12-18 16:06:45 +0700

    add listing stats to email

--- lib/sendgrid/api/entities/email.rb
+++ lib/sendgrid/api/entities/email.rb
@@ -6,7 +6,7 @@ module Sendgrid
       class Email < Entity
 
         attribute :email, :name, :listing_name, :listing_logo_url, 
-                  :listing_communications, :listing_extra_lines,
+                  :listing_communications, :listing_extra_lines, :listing_stats,
                   :listing_info, :listing_categories, :listing_descriptions,
                   :listing_keywords, :listing_relations, :listing_qrcode,
                   :listing_video, :listing_addresses, :listing_profile,
