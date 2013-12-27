64406e895a5e28451f6fe6ce725d4e427b101eef
sinal <meassinal@gmail.com>
2013-12-27 09:41:34 +0700

    add greeting to email

--- lib/sendgrid/api/entities/email.rb
+++ lib/sendgrid/api/entities/email.rb
@@ -4,7 +4,7 @@ module Sendgrid
   module API
     module Entities
       class Email < Entity
-        attribute :email, :name, :full_name, :listing_name, :listing_logo_url, 
+        attribute :email, :name, :greeting, :listing_name, :listing_logo_url, 
                   :listing_communications, :listing_extra_lines, :listing_stats,
                   :listing_info, :listing_categories, :listing_descriptions,
                   :listing_keywords, :listing_relations, :listing_qrcode,
