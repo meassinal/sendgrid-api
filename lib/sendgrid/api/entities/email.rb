ebffda18beebb336a36acf36295b3afdfe325d63
sinal <meassinal@gmail.com>
2014-01-05 23:42:18 +0700

    revert email

--- lib/sendgrid/api/entities/email.rb
+++ lib/sendgrid/api/entities/email.rb
@@ -4,17 +4,15 @@ module Sendgrid
   module API
     module Entities
       class Email < Entity
-        # attribute :email, :name, :greeting, :listing_name, :listing_logo_url, 
-        #           :listing_communications, :listing_extra_lines, :listing_stats,
-        #           :listing_info, :listing_categories, :listing_descriptions,
-        #           :listing_keywords, :listing_relations, :listing_qrcode,
-        #           :listing_video, :listing_addresses, :listing_profile,
-        #           :listing_brochure, :listing_photo_gallery, :listing_people,
-        #           :listing_service_catalog, :listing_product_catalog, :listing_school, 
-        #           :listing_menu, :listing_mobile, :listing_instant_website, :listing_facebook,
-        #           :listing_print_directory
-
-        attribute :email, :name, :greeting, :logo_content, :logo_tip
+        attribute :email, :name, :greeting, :listing_name, :listing_logo_url, 
+                  :listing_communications, :listing_extra_lines, :listing_stats,
+                  :listing_info, :listing_categories, :listing_descriptions,
+                  :listing_keywords, :listing_relations, :listing_qrcode,
+                  :listing_video, :listing_addresses, :listing_profile,
+                  :listing_brochure, :listing_photo_gallery, :listing_people,
+                  :listing_service_catalog, :listing_product_catalog, :listing_school, 
+                  :listing_menu, :listing_mobile, :listing_instant_website, :listing_facebook,
+                  :listing_print_directory
       end
     end
   end
