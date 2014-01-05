383d4611bd583b4c2e0732e850c8c236481b3226
sinal <meassinal@gmail.com>
2014-01-05 23:07:04 +0700

    add logo content and logo tip to email

--- lib/sendgrid/api/entities/email.rb
+++ lib/sendgrid/api/entities/email.rb
@@ -4,16 +4,17 @@ module Sendgrid
   module API
     module Entities
       class Email < Entity
-        attribute :email, :name, :greeting, :listing_name, :listing_logo_url,
-                  :logo_content_tag, :logo_tip_tag, 
-                  :listing_communications, :listing_extra_lines, :listing_stats,
-                  :listing_info, :listing_categories, :listing_descriptions,
-                  :listing_keywords, :listing_relations, :listing_qrcode,
-                  :listing_video, :listing_addresses, :listing_profile,
-                  :listing_brochure, :listing_photo_gallery, :listing_people,
-                  :listing_service_catalog, :listing_product_catalog, :listing_school, 
-                  :listing_menu, :listing_mobile, :listing_instant_website, :listing_facebook,
-                  :listing_print_directory
+        # attribute :email, :name, :greeting, :listing_name, :listing_logo_url, 
+        #           :listing_communications, :listing_extra_lines, :listing_stats,
+        #           :listing_info, :listing_categories, :listing_descriptions,
+        #           :listing_keywords, :listing_relations, :listing_qrcode,
+        #           :listing_video, :listing_addresses, :listing_profile,
+        #           :listing_brochure, :listing_photo_gallery, :listing_people,
+        #           :listing_service_catalog, :listing_product_catalog, :listing_school, 
+        #           :listing_menu, :listing_mobile, :listing_instant_website, :listing_facebook,
+        #           :listing_print_directory
+
+        attribute :email, :name, :greeting, :logo_content, :logo_tip
       end
     end
   end
